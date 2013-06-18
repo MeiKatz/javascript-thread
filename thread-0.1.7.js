bc45ecb13417b54645fdd44b438e185fb675a02b
MeiKatz <appointedaddress@gmail.com>
2013-06-18 17:11:20 +0300

    changed the behavior of Thread
    -- removed Thread.getStatus()
    -- removed addEventListener, removeEventListener, dispatchEvent, postMessage
    -- added constants Thread.RUNNING, Thread.TERMINATED, Thread.ERROR
    -- status now returns 1, 2 or 3 (look at the new constants)
    -- added Thread.id and Thread.group_id
    -- added Thread.send
    -- now using strict mode
    -- added ThreadGroup
    -- all threads now belong to a ThreadGroup, not more, not less
    -- every thread source must now be enclosed in a anonymous function
    -- Thread now throws an exception on error

--- thread-0.1.7.js
+++ thread-0.1.7.js
@@ -1,7 +1,7 @@
 /**
  * @author      Gregor Mitzka (gregor.mitzka@gmail.com)
- * @version     0.1.7
- * @date        2013-08-06
+ * @version     0.2
+ * @date        2013-06-18
  * @licence     beer ware licence
  * ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
@@ -11,34 +11,132 @@
  * ----------------------------------------------------------------------------
  */
 (function() {
+    "use strict";
+
+    //
+    // @param   (string) message: error message
+    //
+    function ThreadError ( message ) {
+        message = message || "unknown error";
+
+        this.toString = function() {
+            return "ThreadError: " + message;
+        };
+    }
+
+    var groups  = {},
+        threads = {};
+
+    function ThreadGroup() {
+        var group_id = ( "group#" + (new Date).valueOf() );
+
+        this.id = function() {
+            return group_id;
+        };
+
+        this.add = function ( thread ) {
+            if ( !( thread instanceof Thread ) ) {
+                throw new ThreadError( "could not add thread, passed argument is not a thread" );
+            }
+
+            threads[ thread.id ] || ( threads[ thread.id ] = thread );
+            groups[ thread.id ] = group_id;
+        };
+
+        this.remove = function ( thread ) {
+            if ( !( thread instanceof Thread ) ) {
+                throw new ThreadError( "could not remove thread, passed argument is not a thread" );
+            }
+
+            groups[ thread.id ] = ThreadGroup.default.id;
+        };
+
+        this.has = function ( thread ) {
+            if ( !( thread instanceof Thread ) ) {
+                throw new ThreadError( "could not check thread, passed argument is not a thread" );
+            }
+
+            return ( groups[ thread.id ] === group_id );
+        };
+        
+        this.length = function() {
+            var id,
+                length = 0;
+            for ( id in groups ) {
+                ( groups[ id ] === group_id ) && ( ++length );
+            }
+            
+            return length;
+        };
+
+        this.kill = function() {
+            var id;
+
+            for ( id in groups ) {
+                ( groups[ id ] === group_id ) && threads[ id ].kill();
+            }
+        };
+
+        this.send = function ( data, success ) {
+            var id;
+
+            for ( id in groups ) {
+                ( groups[ id ] === group_id ) && threads[ id ].send( data, success );
+            }
+        };
+
+        Object.defineProperties( this, {
+            "id": {
+                "get": this.id
+            },
+            
+            "length": {
+                "get": this.length
+            }
+        });
+    }
+
+    ThreadGroup.default = new ThreadGroup;
+
     //
     // @param   (mixed) callback: function, string or instance of HTMLElement
     //
-    Thread = function ( callback ) {
+    function Thread ( callback ) {
+        var thread    = this,
+            thread_id = ( "thread#" + (new Date).valueOf() );
+
         // check if all necessary objects and functions are defined
         if ( !Thread.isSupported ) {
             throw new ThreadError( "could not create thread, not all necessary dependencies are defined" );
         }
-        
+
         // callback is either a function or a script element
         if ( typeof callback === "function" || callback instanceof HTMLScriptElement ) {
             // callback function
             if ( typeof callback === "function" ) {
-                var code = callback.toString().match( /^function\s*\([^\)]*\)\s*\{\s*((\S|\s)*\S)\s*\}$/ )[ 1 ];
+                var code = callback.toString();
             // script element
             } else {
-                var code = callback.textContent.match( /^\s*(.+)\s*$/ )[ 1 ];
+                var code = callback.textContent;
             }
+
+            var source = code.match( /^function\s*\([^\)]*\)\s*\{\s*((\S|\s)*\S)\s*\}$/ );
 
             // code must not be empty
-            if ( code === "" ) {
-                throw new ThreadError( "could not create thread, script does not contain any commands" );
+            if ( source == null || source[ 1 ] === "" ) {
+                throw new ThreadError( "could not create thread, script does not contain any commands or has an invalid structure" );
             }
 
+            source = [
+                "this.addEventListener( \"message\", function ( e ) {", "\n\t",
+                    "var ret = (" + source[ 0 ] + ").call( e.target, e.data );", "\n\t",
+                    "this.postMessage( ret );", "\n",
+                "}, false);"
+            ].join( "" );
+
             // create url for the blob object
-            var url  = window.URL.createObjectURL( new Blob( [ code ], { "type": "application/javascript" } ) );
-            delete code;
-        // file namem
+            var url  = window.URL.createObjectURL( new Blob( [ source ], { "type": "application/javascript" } ) );
+        // file name
         } else if ( typeof callback === "string" ) {
             // determine file name of string
             callback = callback.match( /\S+/ );
@@ -52,87 +150,86 @@
             var url = callback[ 0 ];
         // incorrect value for passed argument
         } else {
-            throw new ThreadError( "could not create thread, passed argument is neither a function nor a script element nor a file" );
+            throw new ThreadError( "could not create thread, passed argument is neither a function nor a script element nor a file uri" );
         }
 
-        // status:
-        // -- true: worker is running
-        // -- false: worker terminated
-        // -- null: worker terminated with an error
-        var status = true;
         var worker = new Worker( url );
+        var status = Thread.RUNNING;
+        console.log(ThreadGroup.default);
+        ThreadGroup.default.add( this );
 
         worker.addEventListener( "error", function ( e ) {
-            // terminate worker
-            worker.terminate();
-            // remove blob objects
-            window.URL.revokeObjectURL( url );
-            status = null;
-        }, false );
+            thread.kill();
+            status = Thread.ERROR;
+            throw new ThreadError( "thread terminated in " + e.filename + " on line " + e.lineno + " with the following message: " + e.message );
+        }, false);
 
-        this.getStatus = function() {
+        this.send = function ( data, success ) {
+            if ( status !== Thread.RUNNING ) {
+                return false;
+            }
+
+            if ( typeof success === "function" ) {
+                worker.addEventListener( "message", function ( e ) {
+                    success.call( thread, e.data );
+                    worker.removeEventListener( this );
+                }, false);
+            }
+
+            worker.postMessage( data );
+        };
+
+        this.status = function() {
             return status;
         };
 
+        this.id = function() {
+            return thread_id;
+        };
+
         this.kill = function() {
-            if ( status ) {
-                worker.terminate();
-                window.URL.revokeObjectURL( url );
-                status = false;
-                return true;
-            } else {
+            if ( status !== Thread.RUNNING ) {
                 return null;
             }
+
+            worker.terminate();
+            window.URL.revokeObjectURL( url );
+            status = Thread.TERMINATED;
+            return true;
         };
 
-        this.postMessage = function ( message ) {
-            if ( status ) {
-                worker.postMessage( message );
-            }
-        };
+        Object.defineProperties( this, {
+            "status": {
+                "get": this.status
+            },
 
-        this.addEventListener = function ( type, listener, useCapture ) {
-            if ( status ) {
-                return worker.addEventListener( type, listener, useCapture );
-            } else {
-                return null;
-            }
-        };
+            "running": {
+                "get": function() {
+                    return ( this.status === Thread.RUNNING );
+                }
+            },
 
-        this.removeEventListener = function ( type, listener, useCapture ) {
-            if ( status ) {
-                return worker.removeEventListener( type, listener, useCapture );
-            } else {
-                return null;
-            }
-        };
+            "terminated": {
+                "get": function() {
+                    return ( this.status !== Thread.RUNNING );
+                }
+            },
 
-        this.dispatchEvent = function ( event ) {
-            if ( status ) {
-                return worker.dispatchEvent( event );
-            } else {
-                return null;
-            }
-        };
-
-        Object.defineProperty( this, "status", {
-            "get": function() {
-                return this.getStatus();
+            "id": {
+                "get": this.id
+            },
+            
+            "group_id": {
+                "get": function() {
+                    return groups[ this.id ];
+                }
             }
         });
+    }
 
-        Object.defineProperty( this, "running", {
-            "get": function() {
-                return ( this.getStatus() === true );
-            }
-        });
-
-        Object.defineProperty( this, "terminated", {
-            "get": function() {
-                return ( this.getStatus() !== true );
-            }
-        });
-    };
+    Thread.RUNNING    = 1;
+    Thread.TERMINATED = 2;
+    Thread.ERROR      = 3;
 
     //
     // @param   (object) thread: instance of Thread
@@ -144,7 +241,7 @@
             return false;
         }
     };
-    
+
     Thread.isSupported = !!(
         window.URL                  &&
         window.URL.createObjectURL  &&
@@ -152,15 +249,8 @@
         window.Blob                 &&
         window.Worker
     );
-
-    //
-    // @param   (string) message: error message
-    //
-    ThreadError = function ( message ) {
-        message = message || "unknown error";
-        
-        this.toString = function() {
-            return "ThreadError: " + message;
-        };
-    };
+    
+    window.Thread      = Thread;
+    window.ThreadError = ThreadError;
+    window.ThreadGroup = ThreadGroup;
 })();
